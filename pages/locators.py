4ad9bc07921a278554814f06526a1b8f951b1468
AKuslin <Sheizer95@mail.ru>
2019-09-04 21:40:39 +0700

    Добавил проверки для login_page: should_be_login_url, should_be_login_form, should_be_register_form

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form1")
+    REG_FORM = (By.ID, "register_form1")
+    EMAIL_LOGIN = (By.NAME, "login-username")
+    PASSWORD_LOGIN = (By.NAME, "login-password")
+    EMAIL_REG = (By.NAME, "registration-email")
+    PASSWORD1_REG = (By.NAME, "registration-password1")
+    PASSWORD2_REG = (By.NAME, "registration-password2")
